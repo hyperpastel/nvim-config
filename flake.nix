cc15c0b15b5db422cbf7dbff2b9becfb48d4bf02
hyperpastel <vmutze04@gmail.com>
2025-06-11 14:55:16 +0200

    Add flake support to config

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix flake for hyperpastel's Neovim config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            lua-language-server
+            stylua
+          ];
+
+          shellHook = ''
+            echo Entered neovim config development shell
+          '';
+        };
+      }
+    );
+}
