746bd5e0e7accadb597d5c1d4e8cdfadb3bd4815
hyperpastel <vmutze04@gmail.com>
2025-07-24 23:59:16 +0200

    fix(treesitter): remove tag in plugin spec
    This forced telescope to an old version that caused issues
    when previewing files for which there was no treesitter
    language installed.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,7 +31,6 @@ local M = {
 	},
 	{
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function ()
 			local builtin = require('telescope.builtin')
